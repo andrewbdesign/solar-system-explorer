c8f8be292141a34a930e43c4c64ac8c2239f89d8
andrewbdesign <contact@andrewbdesign.com>
2020-03-06 08:10:39 +1100

    Neptune splash screen refactor

--- src/components/SplashScreen/Neptune.js
+++ src/components/SplashScreen/Neptune.js
@@ -8,40 +8,37 @@ import PropTypes from 'prop-types';
 import { setPlanet, resetPlanets } from '../../actions/planets';
 
 const RedPlanet = ({ setPlanet, resetPlanets }) => {
+
+  const planets = [
+    '.red-planet',
+    '.blue-planet',
+    '.green-planet',
+    '.orange-planet',
+    '.side-planet',
+    '.bottom-planet',
+    '.saturn-planet',
+    '.uranus-planet',
+  ];
+
   // eslint-disable-next-line
   const plugin = [ScrollToPlugin];
   const [active, setActive] = useState(false);
   useEffect(() => {
     const tl = new TimelineMax();
-    tl.from(
-      '.neptune-planet',
-      1.6,
-      { y: 230, x: 300, scale: 0, ease: Back.easeOut },
-      '1',
-    );
+    tl.from('.neptune-planet', 1.6, { y: 230, x: 300, scale: 0, ease: Back.easeOut }, 1);
   }, []);
 
   const onHandleMouseOver = () => {
     if (!active) {
       const tl = new TimelineMax();
-      tl.to(
-        '.neptune-planet',
-        0.3,
-        { scale: 1.3, x: 0, y: 0, ease: Power1.easeOut },
-        '0',
-      );
+      tl.to( '.neptune-planet', 0.3, { scale: 1.3, x: 0, y: 0, ease: Power1.easeOut }, 0);
     }
   };
 
   const onHandleMouseOut = () => {
     if (!active) {
       const tl = new TimelineMax();
-      tl.to(
-        '.neptune-planet',
-        0.3,
-        { scale: 1, x: 0, y: 0, ease: Power1.easeOut },
-        '0',
-      );
+      tl.to('.neptune-planet', 0.3, { scale: 1, x: 0, y: 0, ease: Power1.easeOut }, 0);
     }
   };
 
@@ -50,80 +47,30 @@ const RedPlanet = ({ setPlanet, resetPlanets }) => {
       const tl = new TimelineMax();
       showPlanets();
       resetPlanets();
-      tl.to(
-        '.moon',
-        0.6,
-        { x: 0, scale: 1, y: 0, ease: Power1.easeInOut },
-        '0',
-      );
-      tl.to(
-        '.scene-overlay, .title-info',
-        0.6,
-        { opacity: 0, ease: Power1.easeInOut },
-        '0',
-      );
-      tl.to('.title', 1, { autoAlpha: 1, ease: Power1.easeInOut }, '0');
+      tl.to('.moon', 0.6, { x: 0, scale: 1, y: 0, ease: Power1.easeInOut }, 0)
+        .to('.scene-overlay, .title-info', 0.6, { opacity: 0, ease: Power1.easeInOut }, 0)
+        .to('.title', 1, { autoAlpha: 1, ease: Power1.easeInOut }, 0);
     } else {
       const tl = new TimelineMax({ delay: 0.6 });
       hidePlanets();
       setPlanet('neptune');
-      TweenLite.to(window, 2, {
-        scrollTo: { y: '.title-info' },
-        ease: Power1.easeInOut,
-        delay: 0.6,
-        force3D: true,
-      });
-      tl.to(
-        '.neptune-planet',
-        1,
-        { scale: 5, x: -80, y: -220, zIndex: 1350, ease: Power1.easeInOut },
-        '0',
-      );
-      tl.to(
-        '.moon',
-        1,
-        { x: 100, scale: 0.7, y: -100, ease: Power1.easeInOut },
-        '0',
-      );
-      tl.to(
-        '.scene-overlay, .title-info',
-        1,
-        { opacity: 1, ease: Power1.easeInOut },
-        '0',
-      );
-      tl.to('.title', 1, { autoAlpha: 0, ease: Power1.easeInOut }, '0');
+      TweenLite.to(window, 2, { scrollTo: { y: '.title-info' }, ease: Power1.easeInOut, delay: 0.6, force3D: true });
+      tl.to('.neptune-planet', 1, { scale: 5, x: -80, y: -220, zIndex: 1350, ease: Power1.easeInOut }, 0)
+        .to('.moon', 1, { x: 100, scale: 0.7, y: -100, ease: Power1.easeInOut }, 0)
+        .to('.scene-overlay, .title-info', 1, { opacity: 1, ease: Power1.easeInOut }, 0)
+        .to('.title', 1, { autoAlpha: 0, ease: Power1.easeInOut }, 0)
     }
     setActive(!active);
   };
 
   const hidePlanets = () => {
-    const planets = [
-      '.red-planet',
-      '.blue-planet',
-      '.green-planet',
-      '.orange-planet',
-      '.side-planet',
-      '.bottom-planet',
-      '.saturn-planet',
-      '.uranus-planet',
-    ];
     const tl = new TimelineMax();
-    tl.to(planets, 1, { autoAlpha: 0, ease: Power1.easeInOut }, '0');
+    tl.to(planets, 1, { autoAlpha: 0, ease: Power1.easeInOut }, 0);
   };
 
   const showPlanets = () => {
-    const planets = [
-      '.red-planet',
-      '.blue-planet',
-      '.green-planet',
-      '.orange-planet',
-      '.side-planet',
-      '.bottom-planet',
-      '.saturn-planet',
-      '.uranus-planet',
-    ];
     const tl = new TimelineMax();
-    tl.to(planets, 0.6, { autoAlpha: 1, ease: Power1.easeInOut }, '0');
+    tl.to(planets, 0.6, { autoAlpha: 1, ease: Power1.easeInOut }, 0);
   };
 
   return (
